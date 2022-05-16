cd069e9b21a5c65e9aca77a009a06bc1bbf61a11
MiguelMarcola <miguelmarcola@gmail.com>
2022-05-16 09:29:32 -0300

    transform input in component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
-import { Button, Flex, FormControl, FormLabel, Input, Stack } from "@chakra-ui/react";
+import { Button, Flex, Stack } from "@chakra-ui/react";
+import { Input } from "../components/Form/Input";
 
-export default function Home() {
+export default function SignIn() {
     return (
         <Flex
             w="100vw"
@@ -19,37 +20,9 @@ export default function Home() {
             >
                 <Stack spacing="4">
 
-                    <FormControl>
-                        <FormLabel htmlFor="email">E-mail</FormLabel>
-                        <Input
-                            name="email"
-                            id="email"
-                            type="email"
-                            focusBorderColor="pink.500"
-                            bgColor="gray.900"
-                            variant="filled"
-                            _hover={{
-                                bgColor: "gray.900"
-                            }}
-                            size="lg"
-                        />
-                    </FormControl>
+                    <Input name="email" label="E-mail" type="email" />
 
-                    <FormControl>
-                        <FormLabel htmlFor="password">Senha</FormLabel>
-                        <Input
-                            name="password"
-                            id="password"
-                            type="password"
-                            focusBorderColor="pink.500"
-                            bgColor="gray.900"
-                            variant="filled"
-                            _hover={{
-                                bgColor: "gray.900"
-                            }}
-                            size="lg"
-                        />
-                    </FormControl>
+                    <Input name="password" label="Senha" type="password" />
 
                 </Stack>
 
