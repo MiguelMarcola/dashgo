008ce642e7df38be6dfc780892102c396a699d47
MiguelMarcola <miguelmarcola@gmail.com>
2022-05-17 12:22:27 -0300

    create pagination component

---
+++ src/pages/users/create.tsx
@@ -0,0 +1,34 @@
+import { Box, Divider, Flex, Heading, SimpleGrid, VStack } from "@chakra-ui/react";
+import { Input } from "../../components/Form/Input";
+import { Header } from "../../components/Header";
+import { Sidebar } from "../../components/Sidebar";
+
+export default function CreateUser() {
+    return (
+        <Box>
+            <Header />
+
+            <Flex w="100%" maxW={1480} my="6" mx="auto" px="6">
+                <Sidebar />
+
+                <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+                    <Heading size="lg" fontWeight="normal"> Criar usuário </Heading>
+
+                    <Divider my="6" borderColor="gray.700" />
+
+                    <VStack spacing="8">
+                        <SimpleGrid minChildWidth={240} spacing="8" w="100%">
+                            <Input name="name" label="Nome completo" />
+                            <Input name="email" type="email" label="E-mail" />
+                        </SimpleGrid>
+
+                        <SimpleGrid minChildWidth={240} spacing="8" w="100%">
+                            <Input name="password" type="password" label="Senha" />
+                            <Input name="password_confirmation" type="password" label="Confirmação da senha" />
+                        </SimpleGrid>
+                    </VStack>
+                </Box>
+            </Flex >
+        </Box >
+    );
+}
